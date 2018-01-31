e796551f53a6cce13624a105ad16f2c40238d50e
interritus2018 <32379661+interritus2018@users.noreply.github.com>
2018-01-30 16:57:14 -1000

    Game 2 Update
    Continue

---
+++ Text101/Assets/TextController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+
+	private enum States {cell, mirror, sheets_0, lock_0, cell_mirror, sheets_1, lock_1, freedom};
+	private States myState;
+	// Use this for initialization
+	void Start () {
+		myState = States.cell;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (myState);
+	}
+
+	void states_cell () {
+		text.text = "You are in a prison cell, and you want to escape. There are some " + 
+				"dirty sheets on the bed, a mirror on the wall, and the door is locked " + 
+				"from the outside.\n\n" + 
+				"Press S to view Sheets, M to view Mirror, and L to view the Lcok";
+		if (Input.GetKeyDown (KeyCode.S)) {
+		}
+			
+	}
+}
